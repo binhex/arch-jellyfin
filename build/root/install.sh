06934379af5908e2ec054ef32178dcddb7c41b79
djaydev <33335484+djaydev@users.noreply.github.com>
2019-01-29 15:06:22 -0500

    Update install.sh
    Add Intel drivers for vaapi hw transcoding with an Intel iGPU.  Requires user to pass --device=/dev/dri:/dev/dri to the container.

--- build/root/install.sh
+++ build/root/install.sh
@@ -19,7 +19,7 @@ mv /tmp/scripts-master/shell/arch/docker/*.sh /root/
 ####
 
 # define pacman packages
-pacman_packages="dotnet-sdk dotnet-runtime imagemagick ffmpeg skia-sharp60 sqlite"
+pacman_packages="dotnet-sdk dotnet-runtime imagemagick ffmpeg skia-sharp60 sqlite libva-intel-driver"
 
 # install compiled packages using pacman
 if [[ ! -z "${pacman_packages}" ]]; then
