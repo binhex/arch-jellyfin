e9d9ccd2b0f427eab355b86d51fee65a31bd02af
binhex <megalith01@gmail.com>
2019-01-29 12:02:56 +0000

    fix env vars and run

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# config below is taken from bash script /usr/bin/jellyfin.sh
-
 # set env variables for jellyfin
-export MONO='/usr/bin/mono'
-export PROGRAM_DATA='/config'
-export FFMPEG='/usr/bin/ffmpeg'
-export FFPROBE='/usr/bin/ffprobe'
+export JELLYFIN_DATA="/tmp"
+export JELLYFIN_ADD_OPTS=""
+export FFMPEG="/usr/bin/ffmpeg"
+export FFPROBE="/usr/bin/ffprobe"
 
 # run jellyfin
-exec '/usr/bin/jellyfin'
+/usr/bin/dotnet /usr/lib/jellyfin/jellyfin.dll -programdata "${JELLYFIN_DATA}" "${JELLYFIN_ADD_OPTS}"
