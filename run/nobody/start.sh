2dc0cbdda9fbafeae02543e0fc0bf3039339e6ce
binhex <megalith01@gmail.com>
2019-04-20 22:12:38 +0100

    remove ffprobe option

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -3,7 +3,6 @@
 # specify argument values
 config_path="/config"
 ffmpeg_file_path="/usr/bin/ffmpeg"
-ffprobe_file_path="/usr/bin/ffprobe"
 
 # move older config to new structure - temporary hack, remove in a few builds time
 mkdir -p /config/config
@@ -12,7 +11,7 @@ mv -f /config/logging.json /config/config/ 2> /dev/null
 mv -f /config/system.xml /config/config/ 2> /dev/null
 mv -f /config/users /config/config/ 2> /dev/null
 
-# move older data to new structure - temporary hack, remove in a few builds time
+# move older data to new structure - temporary hack, remove in a few builds time delme 20190901
 mkdir -p /config/data
 find /config/data -maxdepth 1 -type f -exec mv {} /config/data/data/ \;
 mv -f /config/metadata /config/data/metadata/ 2> /dev/null
@@ -31,5 +30,4 @@ mv -f /config/log*.log /config/logs/ 2> /dev/null
 --cachedir "${config_path}/cache" \
 --configdir "${config_path}/config" \
 --logdir "${config_path}/logs" \
---ffmpeg "${ffmpeg_file_path}" \
---ffprobe "${ffprobe_file_path}"
+--ffmpeg "${ffmpeg_file_path}"
