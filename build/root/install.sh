97e537c6de0807d58d3d24c08ef5f1fdad382c50
binhex <megalith01@gmail.com>
2020-04-27 17:35:54 +0100

    move cleanup of tools to cleanup script

--- build/root/install.sh
+++ build/root/install.sh
@@ -38,9 +38,6 @@ aur_packages="jellyfin"
 # call aur install script (arch user repo)
 source aur.sh
 
-# remove dotket-sdk and yarn as this is only required to build jellyfin
-pacman -Ru dotnet-sdk yarn git --noconfirm
-
 # call custom install script
 # need to install older version of ffmpeg due to bug https://github.com/jellyfin/jellyfin/issues/1269
 source /root/custom.sh
