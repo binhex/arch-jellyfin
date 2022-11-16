defde5cbf942ffada83386e7196f35cd4faa1bdc
binhex <megalith01@gmail.com>
2022-11-16 10:29:09 +0000

    fix for alt aur package

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -2,15 +2,17 @@
 
 # specify argument values
 config_path="/config"
+webdir="/usr/share/jellyfin/web"
 ffmpeg_file_path="/usr/lib/jellyfin-ffmpeg/ffmpeg"
 
 # create folders to store config, data, cache and logs
 mkdir -p /config/config /config/data /config/cache /config/logs
 
 # run jellyfin with arguments
-/usr/bin/dotnet /usr/lib/jellyfin/jellyfin.dll \
+/usr/lib/jellyfin/bin/jellyfin \
 --datadir "${config_path}/data" \
 --cachedir "${config_path}/cache" \
 --configdir "${config_path}/config" \
 --logdir "${config_path}/logs" \
---ffmpeg "${ffmpeg_file_path}"
+--ffmpeg "${ffmpeg_file_path}" \
+--webdir "${webdir}"
